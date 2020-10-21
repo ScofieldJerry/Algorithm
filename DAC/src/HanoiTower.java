ade59a2fdd45c7e6211514a09349e09a3233bac3
scofield <liyunan@sinosoft.com.cn>
2020-10-21 14:26:14 +0800

    分治算法实现汉诺塔

---
+++ DAC/src/HanoiTower.java
@@ -0,0 +1,15 @@
+public class HanoiTower {
+    public static void main(String[] args) {
+        hanoiTower(64,'A','B','C');
+    }
+    //汉诺塔移动方案，使用分治算法
+    public static void hanoiTower(int num, char a, char b, char c){
+        if (num == 1) {
+            System.out.println("第1个盘从" + a + "->" + c);
+        } else {
+            hanoiTower(num - 1, a, c, b);
+            System.out.println("第"+ num +"个盘从" + a + "->" + c);
+            hanoiTower(num - 1, b, a, c);
+        }
+    }
+}
